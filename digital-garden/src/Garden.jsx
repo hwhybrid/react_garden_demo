e0eaef56b1f89f1004b12a051fa5ef06c9fee1b2
hwhybrid <hwhybrid@gmail.com>
2024-05-21 15:35:44 +0200

    modify fertilizer logic to use asynchronous javascriptlogic

--- digital-garden/src/Garden.jsx
+++ digital-garden/src/Garden.jsx
@@ -12,7 +12,9 @@ function Garden() {
 	const [needsWater, setWatered] = useState(true)
     const [fertilizer, setFertilizer] = useState('none');
 	const [fertilizerDescription, setFertilizerDescription] = useState('');
-	
+	const [fertilizerData, setFertilizerData] = useState(null) // State that will be used to store the fetched fertilizer data
+	const [loading, setLoading] = useState(false) // State to manage your loading status
+
 	const waterPlant = () => {setWatered(!needsWater)}
 
 	// This handler function will handle changes in the fertilizer selection. 
@@ -20,6 +22,30 @@ function Garden() {
 	const handleFertilizerChange = (event) => {
 		setFertilizer(event.target.value);
 	} 
+
+	const fetchFertilizerData = async () => {
+		setLoading(true) // Sets loading to true while fetching your data
+		try {
+			await new Promise (resolve => setTimeout(resolve, 1000));
+			const data = JSON.parse(localStorage.getItem('fertilizerData')) || [
+				{id:1, name:'None', description: 'No fertilizer has been applied'},
+				{id:2, name:'Organic', description: 'Organic fertilizer applied. The plant will grow naturally.'},
+				{id:3, name: 'Chemical', description: 'Chemical fertilizer has been applied. The plant might grow faster but cancer is a risk.'}
+			]
+		;
+		setFertilizerData(data)	
+		} catch (error) {
+			console.error('Error fetching fertilizer data...', error) 
+		} finally {
+			setLoading(false) // Sets loading to false after the data has been fetched
+
+		}
+	}
+
+	useEffect(() => {
+		fetchFertilizerData()
+	}, []) //Empty dependency array to ensure the useEffect only tuns once when the component mounts
+
 // The useEffect hook will update the fertilizer description whenever the fertilizer state changes
 	useEffect(
 		// useEffect takes 2 parameters: a function , and an array of dependencies or a dependency array
@@ -65,26 +91,20 @@ function Garden() {
 				</label>
 			</div>
 			<h4>{fertilizerDescription}</h4>
-
-		<div>
-    
-      </div>
-
-
-
-
-
-
-
-
-
-
-			<Plant />
-
-
+			{loading ? (
+				<p>Loading fertilizer data....</p>
+			) : (
+				fertilizerData && (
+				<div key={fertilizer.id}> 
+					<h4>{fertilizer.name}</h4>
+					<p>{fertilizer.description}</p>
+				</div>
+			)
+			)}
 
 		</div>
-	);
+	
+	)
 }
 
 export default Garden;
